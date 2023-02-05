e783ee3761c96b8a820edc6a3491bb09433a62ae
Shadyar Bakr <Shadyar.bakr@proton.me>
2023-02-05 22:16:33 +0300

    refactor: :art: Seperated app and main file

---
+++ lib/src/app.dart
@@ -0,0 +1,23 @@
+// import 'package:bauhouse/src/config/config.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(context, ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bauhouse App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
