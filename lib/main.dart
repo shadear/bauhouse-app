777527ccbc54b7573ce648e6314b9c0ed8b41cba
Shadyar Bakr <Shadyar.bakr@proton.me>
2023-02-05 22:18:35 +0300

    feat: :sparkles: Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:bauhouse/src/app.dart';
+import 'package:bauhouse/src/config/firebase/firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: App(),
